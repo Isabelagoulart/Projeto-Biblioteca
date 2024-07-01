ec74882ca795f5dbc2d5efd5e4c268d745310573
Isabelagoulart <94116289+Isabelagoulart@users.noreply.github.com>
2024-07-01 16:54:56 -0300

    Update LeitorNaoEncontradoException.java

--- src/Exceptions/LeitorNaoEncontradoException.java
+++ src/Exceptions/LeitorNaoEncontradoException.java
@@ -1,6 +1,6 @@
 package Exceptions;
 
-//Tratamendo de erro para quando não encontrar um leitor
+//Erro para quando não encontrar um leitor
 public class LeitorNaoEncontradoException extends Exception{
     public LeitorNaoEncontradoException(String message)
     {
